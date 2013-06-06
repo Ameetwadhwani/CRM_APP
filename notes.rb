f6be26c0024ca13df7eb88552ed8cb609a405605
Bitmaker Labs <bitmakerlabs@cnc.local>
2013-06-06 14:01:52 -0400

    completed notes

--- notes.rb
+++ notes.rb
@@ -1,20 +1,32 @@
 Notes
 
+Summary of understanding:
+________________________________________________________________________________
+
+This simple application is designed to allow multiple users to access the same database to update their own records AND access the records of others.  A sample instance would be bitmaker labs.  Students would each enter their own contact info but also access the contact info of others in the group. 
+
+There is no security layer assumed.  Each user can add, modify, or change any other user records.
+
+
 Contact
 ---------------------------------------------------------------------------------
 User
 1.  As a user I can add contacts to a database that holds their "id", "firstname". "lastname", "email", "notes" which is also 
 
-
+2. As a user I should be prompted to add/delete/etc contacts from within the console
 
 3. As a user I should also be able to modify contact info (i.e. delete, add, change)
 
 4. As a user I should be able to see each one of my contacts easily navigating through the interface (also display nicely)
 
+5. As a user I should be able to see ALL of the contacts in my database 
+
 
 Developer
 
-
+1. As a developer, I should be able to capture the inputs as required by the user
+2. I should be able to store the inputs in a database 
+3. 
 
 
 
@@ -30,13 +42,19 @@ User
 
 Developer
 1. I should create a database class that accepts new contacts
+
 2. I should allow the class to have at least the following methods:  
+
 	"add", 
 	"modify_contact", 
 	"display_all_contacts", 
 	"display_particular_contact", 
 	"display_info_by_attribute"  --> for example display all the firstnames in the database and ONLY their first names (see stretch 3)
 	and "delete_contact" 
+
+3. I should store the data in a local instance (i.e. the data is not saved to a file)
+
+4. 
 
 
 
@@ -47,14 +65,18 @@ Runner
 ---------------------------------------------------------------------------------
 User
 
-.  As a user I should be prompted to add/delete/etc contacts from within the console
-
-
+*  interact with a command line to execute methods developed by the developer
+	
 
 
 
 Developer 
 
+* display all available instructions to the user
+* recieve inputs from the user
+* respond to user inputs with both prompts and actions (methods) as defined in the database class 
+* send messages from the runner to the 'program' in order to execute the program
+* handle exceptions
 
 
 
